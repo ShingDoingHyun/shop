7ba2bfaec3c78e8d4f7113ba3ea80140927e9882
HB04-20 <HB04-20@BIT04-04>
2018-06-14 19:57:30 +0900

    sql수정

--- shopPRJ/WebContent/project.sql
+++ shopPRJ/WebContent/project.sql
@@ -19,4 +19,43 @@ insert into member values(1,'asdf','1234','john','20',true,'01012345678','seoul 
 update member set memberId = 'asdf';
 
 select * from member;
-yongjun0118
+
+
+
+create table product(
+	productNo int auto_increment PRIMARY KEY,
+	productName VARCHAR(200) NOT NULL,
+	productDetail text ,
+	productImage VARCHAR(200),
+	productType VARCHAR(50), 
+	productPrice int,
+	productOpt1 int,
+	productOpt2 int
+);
+
+
+drop table product;
+select * from product;
+
+insert into product (productName, productImage, productPrice) values ('test1' , 'dice1.jpg' ,10000);
+insert into product (productName, productImage, productPrice) values ('test2' , 'dice2.jpg' ,20000);
+insert into product (productName, productImage, productPrice) values ('test3' , 'dice3.jpg' ,1000);
+insert into product (productName, productImage, productPrice) values ('test4' , 'dice4.jpg' ,200);
+insert into product (productName, productImage, productPrice) values ('test5' , 'dice5.jpg' ,100000);
+insert into product (productName, productImage, productPrice) values ('test6' , 'dice6.jpg' ,10000);
+insert into product (productName, productImage, productPrice) values ('test7' , 'dice3.jpg' ,30000);
+insert into product (productName, productImage, productPrice) values ('test8' , 'dice2.jpg' ,70000);
+insert into product (productName, productImage, productPrice) values ('test9' , 'dice1.jpg' ,2000);
+insert into product (productName, productImage, productPrice) values ('test10' , 'dice4.jpg' ,10000);
+insert into product (productName, productImage, productPrice) values ('test11' , 'dice4.jpg' ,10000);
+insert into product (productName, productImage, productPrice) values ('test12' , 'dice2.jpg' ,20000);
+insert into product (productName, productImage, productPrice) values ('test13' , 'dice3.jpg' ,1000);
+insert into product (productName, productImage, productPrice) values ('test14' , 'dice4.jpg' ,200);
+insert into product (productName, productImage, productPrice) values ('test15' , 'dice2.jpg' ,100000);
+insert into product (productName, productImage, productPrice) values ('test16' , 'dice4.jpg' ,10000);
+insert into product (productName, productImage, productPrice) values ('test17' , 'dice6.jpg' ,30000);
+insert into product (productName, productImage, productPrice) values ('test18' , 'dice2.jpg' ,70000);
+insert into product (productName, productImage, productPrice) values ('test19' , 'dice1.jpg' ,2000);
+
+SELECT count(*) From product;
+SELECT * From product limit 4, 3;
